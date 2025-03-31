5f841876c020662deaffac7b61b8457b6fa45fd6
Lucas Henrique Gonçalves Souto <lucashenriquegoncalvessouto1@gmail.com>
2025-03-31 15:09:07 -0300

    feat: Página de conteúdo genérica

---
+++ ContentPage/src/javascript/script.js
@@ -0,0 +1,5 @@
+
+// Função para habilitar a Barra de Pesquisa na tela inicial
+document.getElementById("btn-search").addEventListener("click", function () {
+    document.getElementById("search-container").classList.toggle("active");
+});
