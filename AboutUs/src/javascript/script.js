8f42936eb772d8b114039752b9a0e208156ce035
Daniel Umberto <danielumberto51@gmail.com>
2025-06-19 21:03:54 -0300

    refactor: About us page

--- AboutUs/src/javascript/script.js
+++ AboutUs/src/javascript/script.js
@@ -1,4 +1,17 @@
 // Função para habilitar a Barra de Pesquisa na tela inicial
 document.getElementById("btn-search").addEventListener("click", function () {
-    document.getElementById("search-container").classList.toggle("active");
+    const searchContainer = document.getElementById("search-container");
+    const searchInput = searchContainer.querySelector("input");
+
+    if (!searchContainer.classList.contains("active")) {
+        searchContainer.classList.add("active");
+        searchInput.focus();
+    } else {
+        if (searchInput.value.trim() !== "") {
+            //TESTE
+            console.log("Pesquisando por:", searchInput.value);
+        } else {
+            searchContainer.classList.remove("active");
+        }
+    }
 });
